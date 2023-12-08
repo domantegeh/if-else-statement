987be08d4f652a7fed9200a43f318061313c534f
doman-tegeh <tegeh.add@gmail.com>
2023-12-08 10:25:30 +0800

    Update code

--- index.js
+++ index.js
@@ -25,3 +25,17 @@ if (name) {
 } else {
   console.log("Name is empty!");
 }
+
+// exmaple
+let score = 60;
+if (score >= 90) {
+  console.log("Selamat, Anda mendapatkan nilai A!");
+} else if (score >= 80 && score <= 89) {
+  console.log("Selamat, Anda mendapatkan nilai B!");
+} else if (score >= 70 && score <= 79) {
+  console.log("Selamat, Anda mendapatkan nilai C!");
+} else if (score >= 60 && score <= 69) {
+  console.log("Selamat, Anda mendapatkan nilai D!");
+} else if (score < 60) {
+  console.log("Selamat, Anda mendapatkan nilai E!");
+}
